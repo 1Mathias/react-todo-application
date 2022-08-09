b0b8289a96b2a72ed9d4367f0dd1a009dc70e9ed
1Mathias <mathiasabdisa@gmail.com>
2022-08-09 11:55:20 +0300

    Another Component Added

---
+++ src/Another.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+export default function Another(props){
+    return(
+        <div>
+            Another Component {props.name}
+        </div>
+    )
+}
